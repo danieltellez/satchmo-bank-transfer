ee4e42ef896e7153a5dcf75ae4cee145bf0dae2c
Daniel Tellez <danieltellez@flossfriday.com>
2012-03-21 10:09:47 +0100

    Added templates resources

--- bank_transfer/urls.py
+++ bank_transfer/urls.py
@@ -4,7 +4,7 @@ from satchmo_store.shop.satchmo_settings import get_satchmo_setting
 ssl = get_satchmo_setting('SSL', default_value=False)
 
 urlpatterns = patterns('',
-     (r'^$', '.payment_modules.bank_transfer.views.pay_ship_info', {'SSL':ssl}, 'BANK_TRANSFER_satchmo_checkout-step2'),
-     (r'^confirm/$', '.payment_modules.bank_transfer.views.confirm_info', {'SSL':ssl}, 'BANK_TRANSFER_satchmo_checkout-step3'),
-     (r'^success/$', '.payment_modules.bank_transfer.views.success', {'SSL':ssl}, 'BANK_TRANSFER_satchmo_checkout-success'),
+     (r'^$', 'bank_transfer.views.pay_ship_info', {'SSL':ssl}, 'BANK_TRANSFER_satchmo_checkout-step2'),
+     (r'^confirm/$', 'bank_transfer.views.confirm_info', {'SSL':ssl}, 'BANK_TRANSFER_satchmo_checkout-step3'),
+     (r'^success/$', 'bank_transfer.views.success', {'SSL':ssl}, 'BANK_TRANSFER_satchmo_checkout-success'),
 )
--- bank_transfer/views.py
+++ bank_transfer/views.py
@@ -8,11 +8,16 @@ from satchmo_utils.views import bad_or_missing
 bank_transfer = config_get_group('PAYMENT_BANK_TRANSFER')
 
 def pay_ship_info(request):
-    return payship.base_pay_ship_info(request, bank_transfer, payship.simple_pay_ship_process_form, 'shop/checkout/bank_transfer/pay_ship.html')
+    return payship.base_pay_ship_info(request, 
+                                      bank_transfer, 
+                                      payship.simple_pay_ship_process_form, 
+                                      'shop/checkout/bank_transfer/pay_ship.html')
 pay_ship_info = never_cache(pay_ship_info)
 
 def confirm_info(request):
-    return confirm.credit_confirm_info(request, bank_transfer, 'shop/checkout/bank_transfer/confirm.html')
+    return confirm.credit_confirm_info(request, 
+                                       bank_transfer, 
+                                       'shop/checkout/bank_transfer/confirm.html')
 confirm_info = never_cache(confirm_info)
 
 def success(request):
@@ -25,6 +30,5 @@ def success(request):
     return render_to_response('shop/checkout/bank_transfer/success.html',
                               {'order': order},
                               context_instance=RequestContext(request))
-
 
 success = never_cache(success)
